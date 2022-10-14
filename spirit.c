983d6346a197b960abc9726fb94df70023cccf6b
Itai <itai.kimelman@gmail.com>
2022-10-14 17:58:15 +0300

    updated spirit.c

--- spirit.c
+++ spirit.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUF_LEN 0x20
+#include <unistd.h>
 
 void get_shell(const char *perms) {
     int i;
-    for(i = 1; i < BUF_LEN; i++) {
+    for(i = 1; i < 0x30; i++) {
         if(perms[i] == 0) {
             puts("You aren't privileged enough to control this machine");
             puts("But you can still enjoy our browser");
@@ -20,16 +20,22 @@ char query[0x30] __attribute__ ((aligned (0x10))) = { 0 };
 
 int main() {
     char *perms;
-    unsigned long long query_ptr;
-    
-    puts("Loading the browser...");
+    int i;
+    unsigned long long *a = (unsigned long long *)&query[0x10];
+    char *message = malloc(0x30);
+
+    strcpy(message, "Loading the browser...");
+    setbuf(stdout, NULL);
+
+    puts(message);
+    free(message);
     puts("Welcome to Spirit, the world's most vulnerable web browser");
 
-    free(query + 0x10); 
-    perms = malloc(BUF_LEN);
-    memset(perms, 0, BUF_LEN);
+    read(0, query, 0x40);
+    free(a); 
+    perms = malloc(0x30);
+    memset(perms, 0, 0x30);
     while(1) {
-        fgets(query, BUF_LEN, stdin);
         if(strncmp(query, "WEBSHELL", 8) == 0) {
             get_shell(perms);
         }
@@ -38,7 +44,8 @@ int main() {
         }
         if(strncmp(query, "SEARCH", 6) == 0) {
             puts("This service is unavailable due to maintenance");
-        } 
+        }
+        fgets(query + 0x10, 0x30, stdin);
     }
     free(perms);
     return 0;
